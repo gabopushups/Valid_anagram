da70717af8bd956d3d16358e0fdfab17cba22440
Gabriel H <ghernandezc@unal.edu.co>
2022-04-28 20:07:54 -0500

    Added the first solution using sorted char arrays, and then comparing them

--- src/valid_anagram.java
+++ src/valid_anagram.java
@@ -1,5 +1,90 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class valid_anagram {
-    public static void main(String [] args){
+    private String s1;
+    private String s2;
+
+    public valid_anagram(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    public valid_anagram(){
+    }
+
+    public String getS1() {
+        return s1;
+    }
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+    public String getS2() {
+        return s2;
+    }
+    public void setS2(String s2) {
+        this.s2 = s2;
+    }
+
+    public static void main(String [] args) {
         System.out.println("Done checking project");
+        valid_anagram myValidAnagram = new valid_anagram();
+        myValidAnagram.receiveStrings();
+        myValidAnagram.processStrings();
+    }
+
+    public void receiveStrings(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please insert the first string");
+        String s1 = in.nextLine();
+        System.out.println("Received String: "+ s1);
+        this.setS1(s1);
+        System.out.println("Please insert the second string");
+        String s2 = in.nextLine();
+        System.out.println("Received String: "+ s2);
+        this.setS2(s2);
+    }
+
+    public void processStrings(){
+        if ((this.s1== null) || (this.s2==null)){
+            System.out.println("No Strings given");
+            return;
+        }
+
+        if (this.s1.length()!=this.s2.length()){
+            System.out.println("FALSE");
+            return;
+        }
+
+        // Get the char array version of each string
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+
+        // **** **** **** **** SOLUTION 1 **** **** **** **** **** **** ****
+        //
+        // Order both character arrays, and compare them
+        //
+        // The Arrays.sort uses a Dual-Pivot Quicksort by Vladimir Yaroslavskiy, Jon Bentley, and Joshua Bloch
+        // Which has a complexity of O(n log(n))
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html#sort-char:A-
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        System.out.println(this.s1 + " --> " +new String(ch1));
+        System.out.println(this.s2 + " --> " +new String(ch2));
+
+        // Using Arrays.equals(ch1,ch2)
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html#equals-char:A-char:A-
+
+        boolean result = Arrays.equals(ch1,ch2) ?true:false;
+        System.out.println("Are they anagrams? " +result);
+
+        // **** **** **** **** SOLUTION 2 **** **** **** **** **** **** ****
+        //
+        // Use HashMaps
+
+        //
     }
 }
